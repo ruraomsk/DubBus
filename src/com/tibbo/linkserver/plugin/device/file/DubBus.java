c72c3bd883c92eccfb9e157d835029a54a7c96f8
Rusinov Yury <rusinovyury@gmail.com>
2016-07-21 15:11:14 +0600

    Revert "Revert "start""
    This reverts commit 8732aa973b4fdc11e23e77b2092405194de2eada.

---
+++ src/com/tibbo/linkserver/plugin/device/file/DubBus.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tibbo.linkserver.plugin.device.file;
+
+/**
+ *
+ * @author Rura
+ */
+public class DubBus {
+    
+    public int lenCoils;
+    public int lenDI;
+    public int lenIR;
+    public int lenHR;
+    public long step;
+    DubBus(){
+        
+    }
+}
